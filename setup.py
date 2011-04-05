d15154d767fcb30cc5819d1bae69273e970c3dc9
Dustin Lacewell <dlacewell@gmail.com>
2011-04-05 16:28:59 -0500

    Updated packaging

--- setup.py
+++ setup.py
@@ -4,8 +4,11 @@ from distutils.core import setup
 
 setup(
     name='pin-pip',
-    version='0.0dev',
+    version='0.1',
     packages=['pin.plugins'],
-    license='Creative Commons Attribution-Noncommercial-Share Alike license',
-    long_description=open('README').read(),
+    author="Dustin Lacewell",
+    author_email="dlacewell@gmail.com",
+    url="https://github.com/dustinlacewell/pin-pip",
+    description="Plugins providing basic pip functionality for pin.",
+    long_description=open('README.markdown').read(),
 )
