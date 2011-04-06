9a0e0ac4434a704dd9413de41531b4f5bf5765a5
Dustin Lacewell <dlacewell@gmail.com>
2011-04-06 11:17:43 -0500

    Added dependencies

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ from distutils.core import setup
 
 setup(
     name='pinpip',
-    version='0.1',
+    version='0.1.1',
     packages=['pin.plugins'],
-    requires=["pip", "pin"],
+    install_requires=["pip", "pin"],
     provides=["pinpip"],
     author="Dustin Lacewell",
     author_email="dlacewell@gmail.com",
